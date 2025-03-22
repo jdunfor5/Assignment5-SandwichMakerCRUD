3116adaff842fca895fd2265d5c3368a2f5c3a71
Jacob Dunford <jdunfor5@charlotte.edu>
2025-03-22 19:08:52 -0400

    Add CRUD controller for Sandwiches table

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -15,17 +15,17 @@ def create(db: Session, sandwich):
 def read_all(db: Session):
     return db.query(models.Sandwich).all()
 
-def read_one(db: Session, sandwich_id):
+def read_one(db: Session, sandwich_id: int):
     return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
 
-def update(db: Session, sandwich_id, sandwich):
+def update(db: Session, sandwich_id: int, sandwich):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
     update_data = sandwich.model_dump(exclude_unset=True)
     db_sandwich.update(update_data, synchronize_session=False)
     db.commit()
     return db_sandwich.first()
 
-def delete(db: Session, sandwich_id):
+def delete(db: Session, sandwich_id: int):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
     db_sandwich.delete(synchronize_session=False)
     db.commit()
