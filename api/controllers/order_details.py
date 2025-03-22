c995c8481c8ad88d3ba0c666c26239061575e7f8
Jacob Dunford <jdunfor5@charlotte.edu>
2025-03-22 19:10:42 -0400

    Add CRUD controller for OrderDetails table

--- api/controllers/order_details.py
+++ api/controllers/order_details.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from fastapi import Response, status
+from fastapi import status, Response
 from ..models import models, schemas
 
 def create(db: Session, order_detail):
@@ -16,18 +16,18 @@ def create(db: Session, order_detail):
 def read_all(db: Session):
     return db.query(models.OrderDetail).all()
 
-def read_one(db: Session, detail_id):
-    return db.query(models.OrderDetail).filter(models.OrderDetail.id == detail_id).first()
+def read_one(db: Session, order_detail_id: int):
+    return db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id).first()
 
-def update(db: Session, detail_id, order_detail):
-    db_order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == detail_id)
+def update(db: Session, order_detail_id: int, order_detail):
+    db_order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id)
     update_data = order_detail.model_dump(exclude_unset=True)
     db_order_detail.update(update_data, synchronize_session=False)
     db.commit()
     return db_order_detail.first()
 
-def delete(db: Session, detail_id):
-    db_order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == detail_id)
+def delete(db: Session, order_detail_id: int):
+    db_order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id)
     db_order_detail.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
